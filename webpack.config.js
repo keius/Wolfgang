7aef0793170eb3dbd60c6e3bac635459296e1e87
Greg <waterparkmelon@gmail.com>
2017-08-01 13:03:54 -0700

    working on canvas

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./lib/main.js",
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+};
